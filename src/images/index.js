ecfb47210c6e2b6d39c99b34d0ba643b566def33
Ethan <ethanling1@gmail.com>
2019-12-03 09:42:02 -0500

    fixed image link

--- src/images/index.js
+++ src/images/index.js
@@ -1,20 +1,24 @@
-const images = [	
-	{
-		title: 'Indians',
-		link: 'https://upload.wikimedia.org/wikipedia/en/5/52/Cleveland_Indians_logo.svg',
-	},
-	{
-		title: 'Browns',
-		link: 'https://usatftw.files.wordpress.com/2015/02/helmet_top_center1.png?w=660&h=509&crop=1&zoom=2',
-	},
-	{
-		title: 'Cavs',
-		link: 'https://i.cdn.turner.com/nba/nba/.element/media/2.0/teamsites/cavaliers/images/170531-global-logo.png',
-	},
-	{
-		title: 'Monsters',
-		link: 'http://content.sportslogos.net/logos/2/2451/full/ewlcf5ufdepniy91og6q.png',
-	},
+const images = [
+    {
+        title: "Indians",
+        link:
+            "https://upload.wikimedia.org/wikipedia/en/5/52/Cleveland_Indians_logo.svg"
+    },
+    {
+        title: "Browns",
+        link:
+            "https://usatftw.files.wordpress.com/2015/02/helmet_top_center1.png?w=660&h=509&crop=1&zoom=2"
+    },
+    {
+        title: "Cavs",
+        link:
+            "https://i.cdn.turner.com/nba/nba/.element/media/2.0/teamsites/cavaliers/images/170531-global-logo.png"
+    },
+    {
+        title: "Monsters",
+        link:
+            "https://upload.wikimedia.org/wikipedia/en/thumb/e/e6/Cleveland_Monsters_logo.svg/1200px-Cleveland_Monsters_logo.svg.png"
+    }
 ];
 
 export default images;
